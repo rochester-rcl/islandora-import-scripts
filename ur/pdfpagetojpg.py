bb67459f54e20b557af5b4c9c3fb041e62bc2106
nate-rcl <nsarr@library.rochester.edu>
2019-07-31 16:07:58 -0400

    update for using wand only - subprocess no longer works

--- ur/pdfpagetojpg.py
+++ ur/pdfpagetojpg.py
@@ -26,14 +26,28 @@ def convert_pdf_page(sourcePdf,  baseWidth, destination, pageNum = 0, quality = 
        ]
 
         #run the command
+        print("HELLO")
         print("command = " + ' '.join(command))
         subprocess.run(command, shell=True)
 
+def convert_pdf_page2(sourcePdf,  baseWidth, destination, pageNum = 0, quality = 60, colorspace="rgb", dpi = 72):
+    
+
+    # create format of  "DRIVE:\\dir1\\dir2\\file.pdf[pageNumber]"
+    with Image(filename=sourcePdf + "[" + str(pageNum) + "]") as img:
+       img.compression_quality = quality
+       img.alpha_channel = False
+       img.resolution = (dpi,dpi)
+       img.transform_colorspace(colorspace)
+       img.transform(resize="{}x".format(baseWidth))
+       img.save(filename=destination)
+        
+
 def main():
-    convert_pdf_page(sourcePdf = "F:\\test\\1981_APRIL.pdf", 
+    convert_pdf_page2(sourcePdf = "/Users/nsarr/git-projects/campus-times-reports/output/0001/OBJ.pdf", 
         baseWidth = 400, 
         quality = 60,
-        destination ="F:\\test\\1981_APRIL.jpg" )
+        destination ="/Users/nsarr/git-projects/campus-times-reports/output/0002.jpg" )
 
 if __name__ == '__main__':
     main()
